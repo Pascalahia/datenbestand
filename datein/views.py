360dc729863a04b64085837d3c05cb6c4bd32eb4
Pascalahia <pascalahiagble@gmail.com>
2020-09-22 23:26:22 +0200

    Templates and static(Front-end)

--- datein/views.py
+++ datein/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+from .models import Dataset
+
 
 def index(request):
-    return HttpResponse("Hello, this my first views for the statistic")
+    template = loader.get_template('datein/index.html')
+    context = dict()
+    return HttpResponse(template.render(context, request))
+
+    # return HttpResponse("Hello, this my first views for the statistic")
 # Create your views here.
