0e46652b72b21884aedaf81ac3d6b4452aa4b9c6
soner <sonercan.oez@web.de>
2020-10-22 20:52:45 +0200

    Berechnungen Hinzugefügt

--- datein/static/js/base.js
+++ datein/static/js/base.js
@@ -1,5 +1,127 @@
-/*function get_element(){
-          var order = $('inf')
+function myfunction(){
 
+  //  var start = $("#q1");
+   // start.html("<h3>Quality of gender:"+  JSON.parse(mydata_object[0])+ "</h3>");
+  //  start.show();
+    var start = mydata_object;
+    console.log(start)
+    //console.log(start["information"])
+
+
+    data = start["information"];
+    var qlist =["a","b","c"];
+    $("#q1").html(qlist);
+    $('#q1').css({'visibility': 'visible'});
+    for(var key in data){
+        if (key == "gender_information"){
+            console.log("was gefunden undzwar " + key );
+            var innerdata = data[key]["information"];
+            var N = Object.keys(innerdata).length;
+            var n = 0;
+            var innerS = 0;
+
+            for (var index in innerdata ){
+                n = n+ innerdata[index];
+            }
+
+            var m = n/N;
+
+            for (var index in innerdata ){
+                innerS = innerS + ((innerdata[index]-m)**2);
+            }
+            var S = Math.sqrt((1/N)*innerS)
+            console.log(S);
+            console.log((data[key]["information"]));
+            console.log("N ist " + N);
+            console.log("n ist " + n);
+            console.log("m ist " + m);
+
+            var Smax = ((N-1)*m+n-m)*(1/N);
+            var finquality = (Smax-S)/Smax;
+            console.log("Die Qualität ist " + finquality);
+        }else if( key == "age_information"){
+            console.log("was gefunden undzwar " + key);
+            var innerdata = data[key]["information"];
+            var N = Object.keys(innerdata).length;
+            var n = 0;
+            var innerS = 0;
+
+            for (var index in innerdata ){
+                n = n+ innerdata[index];
+            }
+
+            var m = n/N;
+
+            for (var index in innerdata ){
+                innerS = innerS + ((innerdata[index]-m)**2);
+            }
+            var S = Math.sqrt((1/N)*innerS)
+            console.log(S);
+            console.log((data[key]["information"]));
+            console.log("N ist " + N);
+            console.log("n ist " + n);
+            console.log("m ist " + m);
+
+            var Smax = ((N-1)*m+n-m)*(1/N);
+            var finquality = (Smax-S)/Smax;
+            console.log("Die Qualität ist " + finquality);
+        }else if (key == "income_information"){
+            console.log("was gefunden undzwar " + key);
+            var innerdata = data[key]["information"];
+            var N = Object.keys(innerdata).length;
+            var n = 0;
+            var innerS = 0;
+
+            for (var index in innerdata ){
+                n = n+ innerdata[index];
+            }
+
+            var m = n/N;
+
+            for (var index in innerdata ){
+                innerS = innerS + ((innerdata[index]-m)**2);
+            }
+            var S = Math.sqrt((1/N)*innerS)
+            console.log(S);
+            console.log((data[key]["information"]));
+            console.log("N ist " + N);
+            console.log("n ist " + n);
+            console.log("m ist " + m);
+
+            var Smax = ((N-1)*m+n-m)*(1/N);
+            var finquality = (Smax-S)/Smax;
+            console.log("Die Qualität ist " + finquality);
+        }else if (key == "geography_information"){
+            console.log("was gefunden undzwar " + key);
+            var innerdata = data[key]["information"];
+            var N = Object.keys(innerdata).length;
+            var n = 0;
+            var innerS = 0;
+
+            for (var index in innerdata ){
+                n = n+ innerdata[index];
+            }
+
+            var m = n/N;
+
+            for (var index in innerdata ){
+                innerS = innerS + ((innerdata[index]-m)**2);
+            }
+            var S = Math.sqrt((1/N)*innerS)
+            console.log(S);
+            console.log((data[key]["information"]));
+            console.log("N ist " + N);
+            console.log("n ist " + n);
+            console.log("m ist " + m);
+
+            var Smax = ((N-1)*m+n-m)*(1/N);
+            var finquality = (Smax-S)/Smax;
+            console.log("Die Qualität ist " + finquality);
+        }else{}
+    }
+   // var order = $('#q1').text()
+    //console.log(order)
+    //console.log(order["gender_information"])
 }
-*/
+
+
